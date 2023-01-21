658e7b1050d914fb137aae8fec2129d6421e32b4
shubhojeet1 <103964132+shubhojeet1@users.noreply.github.com>
2023-01-21 14:26:13 +0530

    mock12

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connect = require("./config/db");
+const app = express();
+const User = require("./features/user/user.route");
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/users", User);
+
+
+
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Route of Mock-Revision Backend");
+});
+
+app.listen(PORT, async () => {
+  await connect();
+});
+
